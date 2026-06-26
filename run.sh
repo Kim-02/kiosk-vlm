0fafe8e6945519b7007680a17dbbc77770be9fb6
Kim-02 <curry2002@koreatech.ac.kr>
2026-06-26 16:47:33 +0900

    실행파일 수정

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-# VLM venv 활성화 (transformers 등 필요 시)
+# VLM venv 활성화
 source /media/ds/DATA/duego-server-venv/.duego-vlm-server/bin/activate
 
 export EDGELLM_PLUGIN_PATH="/home/ds/edge_llm/TensorRT-Edge-LLM/build/libNvInfer_edgellm_plugin.so"
 
 cd "$(dirname "$0")"
-python3 main.py
+
+python3 main.py 2> >(awk '
+  !/FMHA DEBUG/ &&
+  !/FMHA SELECTED/ &&
+  !/FMHA FUNC ATTR: result=0/ &&
+  !/Switching optimization profile/
+' >&2)
